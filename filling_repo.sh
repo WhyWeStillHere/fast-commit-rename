44314e37686fda617e304415e3daac885bf8b3cb
WhyWeStillHere <36025803+WhyWeStillHere@users.noreply.github.com>
2020-05-16 14:49:38 +0300

    Add comments and updated script

--- filling_repo.sh
+++ filling_repo.sh
@@ -5,11 +5,16 @@ if [ -z $1 ]; then
 	exit 1
 fi
 
+mkdir $1
 cd $1
+
+git init
 
 for value in {1..20}
 do
-	echo "New file ${value}" > new_file_$value
+	dd if=/dev/urandom of=new_file_$value bs=1048576 count=10
 	git add new_file_$value
+	dd if=/dev/urandom of=new_file2_$value bs=1048576 count=10
+	git add new_file2_$value
 	git commit -m "Add file ${value}"
 done
--- main.c
+++ main.c
@@ -20,12 +20,13 @@ int main(int argc, char* argv[]) {
     if (argc > 3) {
         repo_name = argv[3];
     } else {
-        repo_name = malloc(sizeof("./"));
+        repo_name = malloc(sizeof("./"));q
         strcpy(repo_name, "./");
     }
     int error;
 
     git_libgit2_init();
+    // Открываем текущий репозиторий
     git_repository *repo = NULL;
     error = git_repository_open(&repo, repo_name);
     if (error < 0) {
@@ -35,31 +36,36 @@ int main(int argc, char* argv[]) {
         free(repo_name);
     }
 
+    // Получаем ссылку на текущую ветку
     git_reference* head_branch_reference;
     error = git_repository_head(&head_branch_reference, repo);
     if (error < 0) {
         HandleError(error);
     }
+    // Получаем имя текущей ветки
     const char* branch_name;
     error = git_branch_name(&branch_name, head_branch_reference);
     if (error < 0) {
         HandleError(error);
     }
 
+    // Создаем объект типа walker для посещения коммитов
     git_revwalk *walker;
     error = git_revwalk_new(&walker, repo);
     if (error < 0) {
         HandleError(error);
     }
-
+    // Хотим просматривать коммиты, от самого раннего до последнего 
     git_revwalk_sorting(walker, GIT_SORT_REVERSE);
 
+    // Получаем ссылку на коммит, который нам передали
     git_object* commit_object;
     error = git_revparse_single(&commit_object, repo, commit_ref);
     git_commit* commit_to_change = (git_commit*)commit_object;
 
     git_oid current_commit_oid = *git_commit_id(commit_to_change);
 
+    // Прячем наш коммит и всех его предков
     git_revwalk_hide(walker, &current_commit_oid);
     git_revwalk_push_head(walker);
 
@@ -71,12 +77,14 @@ int main(int argc, char* argv[]) {
     git_oid previous_commit_oid;
     size_t is_message_changed = 0;
 
+    // Заменяем текущий коммит и изменяем историю коммитов идя от него
     do {
         error = git_commit_lookup(&current_commit, repo, &current_commit_oid);
         if (error < 0) {
             HandleError(error);
         }
 
+        // Получаем список предков нашего коммита
         size_t parent_count = git_commit_parentcount(current_commit);
         git_commit* parents[parent_count];
 
@@ -87,6 +95,7 @@ int main(int argc, char* argv[]) {
             }
             const git_oid* parent_commit_oid = git_commit_id(parents[i]);
 
+            // Заменяем старый коммит, на коммит из актуальной ветки (с измененной историей)
             if ((previous_changed_commit != NULL) &&
                 (git_oid_cmp(parent_commit_oid, &previous_commit_oid) == 0)) {
                 git_commit_free(parents[i]);
@@ -105,6 +114,7 @@ int main(int argc, char* argv[]) {
             message = new_message;
         }
 
+        // Создаем новый коммит
         error = git_commit_create(
                 &new_commit_oid,
                 repo,
@@ -123,7 +133,7 @@ int main(int argc, char* argv[]) {
 
         git_tree_free(tree);
 
-        //Fixed commit free there
+        //Деалоцируем загруженные parent коммиты
         for (int i = 0; i < parent_count; ++i) {
             git_commit_free(parents[i]);
         }
@@ -136,22 +146,27 @@ int main(int argc, char* argv[]) {
 
     git_revwalk_free(walker);
 
+    // "Отсоединем" HEAD от ветки
     git_reference* new_branch_ref;
     error = git_repository_set_head_detached(repo, &new_commit_oid);
     if (error < 0) {
         HandleError(error);
     }
 
+    // Заменяем старую ветку, на новую, которую мы создали
     error = git_branch_create(&new_branch_ref, repo, branch_name, previous_changed_commit, 1);
     if (error < 0) {
         HandleError(error);
     }
     git_reference_free(head_branch_reference);
 
+    // Устанавливаем HEAD на новую ветку
     error = git_repository_set_head(repo, git_reference_name(new_branch_ref));
     if (error < 0) {
         HandleError(error);
     }
+    // Подгружаем изменения в working area
+    // (Ничего не должно измениться, т.к. мы только переименновали коммиты)
     error = git_checkout_head(repo, NULL);
     if (error < 0) {
         HandleError(error);
